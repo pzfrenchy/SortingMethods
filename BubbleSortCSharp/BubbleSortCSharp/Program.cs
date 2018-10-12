f699f4d22432edd3680ce7bb47a5862c78bba47e
Dan French <dan.french82@gmail.com>
2018-10-12 21:32:00 +0100

    Bubblesort c# example added

---
+++ BubbleSortCSharp/BubbleSortCSharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortCSharp
+{
+    class Program
+    {
+        static int[] nums = { 4, 2, 7, 3, 1 };
+
+        static void Main(string[] args)
+        {
+            foreach (var n in nums)
+            {
+                Console.Write(n);
+            }
+            Console.WriteLine();
+
+            int[] sortedList = bubbleSort(nums);
+            foreach (var n in sortedList)
+            {
+                Console.Write(n);
+            }
+            Console.ReadLine();
+        }
+
+        static int[] bubbleSort(int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = 0; j < list.Length - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
